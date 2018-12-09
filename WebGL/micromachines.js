0b706cd9e363726335fc08cfe8069bc5436c18d5
ABCRic <ABCRic@users.noreply.github.com>
2018-12-09 22:44:38 +0000

    Add bump mapping for cube

--- WebGL/micromachines.js
+++ WebGL/micromachines.js
@@ -5,8 +5,34 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+
+var uniforms = {
+    color: {value: new THREE.Color(0xFFFF00)},
+    baseTexture: {value: new THREE.TextureLoader().load("bump.png")},
+    normalTexture: {value: new THREE.TextureLoader().load("bump.png")},
+    LightSource: {value:
+        {pos: new THREE.Vector3(2, 2, 2),
+         ambient: new THREE.Color(0x111111),
+         diffuse: new THREE.Color(0xeeeeee),
+         specular: new THREE.Color(0xffffff)}},
+    Material: {value:
+        {diffuse: new THREE.Vector3(1, 0, 0),
+         specular: new THREE.Vector3(1, 0, 1),
+         shininess: 25}},
+};
+var material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById('bumpMappingVertexShader').textContent,
+    fragmentShader: document.getElementById('bumpMappingFragmentShader').textContent
+});
+
+spotLight = new THREE.SpotLight( 0xffffbb, 2 );
+spotLight.position.set( 0.5, 0, 1 );
+spotLight.position.multiplyScalar( 700 );
+scene.add( spotLight );
+
+THREE.BufferGeometryUtils.computeTangents(geometry); // compute tangents for bump mapping shader
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
